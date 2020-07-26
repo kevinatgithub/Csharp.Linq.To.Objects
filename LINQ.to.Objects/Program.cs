104e9c6c1a1c9fd11180ab1f5b483244c1cfed11
Kevin <kevinc@magenic.com>
2020-07-26 15:01:17 +0800

    First Commit, project created.

---
+++ LINQ.to.Objects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ.to.Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
